46496f621404ae6539510217a1510c90cfd38d1a
Vishnu Ravva <96168813+vishnuravva@users.noreply.github.com>
2024-02-22 10:03:10 +0530

    Update Diff.java

--- Diff.java
+++ Diff.java
@@ -1,7 +1,7 @@
 public class Sum{
 public static void main(String[] args){
 System.out.println("hello git");
-int a = 4, =4;
-System.out.prinln(a-b);
+int a = 4, b=4;
+System.out.println(a-b);
 }
 }
