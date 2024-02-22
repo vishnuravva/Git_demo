e5bd2e6615e6650081ec77460343664387b92300
Vishnu Ravva <96168813+vishnuravva@users.noreply.github.com>
2024-02-22 10:04:47 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 public class Sum{
 public static void main(String[] args){
 System.out.println("hello git from learning pull commands");
-System.out.prinln("hello git pull");
+System.out.println("hello git pull");
 }
 }
